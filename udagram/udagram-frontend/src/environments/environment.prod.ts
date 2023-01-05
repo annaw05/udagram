2d80d170bbd810fccb03682da16e5d65198b62ff
annaw05 <115083810+annaw05@users.noreply.github.com>
2023-01-06 00:09:23 +0100

    fixed linting

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
+  appName: 'Udagram',
   apiHost: 'https://udagram-api-dev22222222222222.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
