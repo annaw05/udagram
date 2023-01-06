7f84bb890bb65c903d2e0b158177aeeacdc8e944
annaw05 <115083810+annaw05@users.noreply.github.com>
2023-01-06 20:34:31 +0100

    test

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -17,8 +17,8 @@ export const config = {
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
-  //port: Number(process.env.PORT),
-  //url: process.env.URL,
+  port: Number(process.env.PORT),
+  url: process.env.URL,
 
   jwt: {
     secret: process.env.JWT_SECRET,
