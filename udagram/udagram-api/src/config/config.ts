1f1f0a8b2d691939ab4b41a752003dbc1fb09a20
annaw05 <115083810+annaw05@users.noreply.github.com>
2023-01-06 22:54:49 +0100

    connection

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -11,18 +11,18 @@ export const config = {
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   host: process.env.POSTGRES_HOST,
-  
-  dialect: "postgres",
-  
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
-  port: Number(process.env.PORT),
   url: process.env.URL,
-
   jwt: {
     secret: process.env.JWT_SECRET,
   },
+  // dialect: "postgres",
+
+
+  // port: Number(process.env.PORT),
+
 
   aws_access_key: process.env.AWS_ACCESS_KEY_ID,
   aws_secret_key: process.env.AWS_SECRET_ACCESS_KEY,
